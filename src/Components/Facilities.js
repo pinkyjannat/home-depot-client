356ec7042914eed48a80b4f9e690f7f8c47beff1
Jannatul Ferdous <jannatpinky92@gmail.com>
2022-05-26 02:30:38 -0400

    admin added

--- src/Components/Facilities.js
+++ src/Components/Facilities.js
@@ -6,10 +6,10 @@ import { FaWarehouse } from 'react-icons/fa';
 
 const Facilities = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <div className=" text-center fs-3">
+        <div className='grid lg:grid-cols-3  md:grid-cols-3  gap-8 grid-cols-3 my-10'>
+           
             <div class="card lg:card-side bg-base-100 shadow-xl">
-                <figure><MdOutlineLocalShipping  size={"70px"}></MdOutlineLocalShipping></figure>
+                <figure className="pl-2"><MdOutlineLocalShipping  size={"70px"}></MdOutlineLocalShipping></figure>
                 <div class="card-body">
                     <h2 class="card-title">World Wide Shippping</h2>
                     <p>We are shipping our product all around the world.</p>
@@ -17,7 +17,7 @@ const Facilities = () => {
                 </div>
             </div>
             <div class="card lg:card-side bg-base-100 shadow-xl">
-                <figure><FaWarehouse size={"70px"}></FaWarehouse></figure>
+                <figure className="pl-2"><FaWarehouse size={"70px"}></FaWarehouse></figure>
                 <div class="card-body">
                     <h2 class="card-title">We Stocks more than over!</h2>
                     <p>We usually stocks our product all aronud the year !! </p>
@@ -25,14 +25,14 @@ const Facilities = () => {
                 </div>
             </div>
             <div class="card lg:card-side bg-base-100 shadow-xl">
-                <figure className=''><FaPeopleArrows size={"70px"}></FaPeopleArrows></figure>
+                <figure className="pl-2" ><FaPeopleArrows size={"70px"}></FaPeopleArrows></figure>
                 <div class="card-body">
                     <h2 class="card-title">Shipping available !!</h2>
                     <p>We shipp our products when wholesale option in available!</p>
                     
                 </div>
             </div>
-            </div>
+            
         </div>
 
     );
